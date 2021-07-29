bd19f3b3996ac8d720134ebb2668692b0a5057e7
Aleksandar Zivanovic <56928480+aleksandar-zivanovic@users.noreply.github.com>
2021-07-29 14:36:38 +0200

    Create config.php

---
+++ classes/config.php
@@ -0,0 +1,25 @@
+<?php
+
+/* Podesavanje SMTP-a za slanje emailova */
+
+// configuracija za mailtrap.io
+//class Config{
+//    const SMTP_HOST = 'smtp.mailtrap.io';
+//    const SMTP_PORT = '2525';
+//    const SMTP_USER = 'enter_your_username';
+//    const SMTP_PASSWORD = 'enter_your_password';
+//}
+
+
+class Config {
+
+    const SMTP_HOST = 'SMTP host';
+    const SMTP_PORT = 'SMTP port';
+    const SMTP_USER = 'SMTP korisnicko ime';
+    const SMTP_PASSWORD = 'SMTP sifra';
+    const FROM_EMAIL = 'email sa koga se salju emailovi';
+    const FROM_NAME = 'naizv u FROM-u emaila';
+
+}
+
+?>
