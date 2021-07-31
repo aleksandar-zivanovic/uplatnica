06a22829f276a29d265bdda87ba65387da8baf25
Aleksandar Zivanovic <acas_mail@yahoo.com>
2021-07-31 03:36:50 +0200

    Update uplatnica.sql
    U bazu je dodato polje za jedinstveni kod uplatnice. Zaboravljeno da se doda u prethodnom komitu, da bi skripta mogla ispravno da radi

--- baza/uplatnica.sql
+++ baza/uplatnica.sql
@@ -1,47 +1,30 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19  Distrib 10.4.20-MariaDB, for Win64 (AMD64)
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 04, 2021 at 01:38 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.2
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: uplatnica
+-- ------------------------------------------------------
+-- Server version	10.4.20-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `uplatnica`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `primalac`
---
-
-CREATE TABLE `primalac` (
-  `id_primaoca` int(11) NOT NULL,
-  `naziv_primaoca` varchar(255) NOT NULL,
-  `racun_primaoca` varchar(50) NOT NULL,
-  `id_uplatioca` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `sacuvane_uplatnice`
 --
 
+DROP TABLE IF EXISTS `sacuvane_uplatnice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sacuvane_uplatnice` (
-  `id_uplate` int(11) NOT NULL,
+  `id_uplate` int(11) NOT NULL AUTO_INCREMENT,
+  `kod_uplatnice` varchar(255) NOT NULL,
   `su_id_uplatioca` int(11) NOT NULL,
   `su_uplatilac` varchar(255) NOT NULL,
   `su_svrha` varchar(255) NOT NULL,
@@ -52,30 +35,21 @@ CREATE TABLE `sacuvane_uplatnice` (
   `su_racun_primaoca` varchar(25) NOT NULL,
   `su_model` varchar(5) NOT NULL,
   `su_poz_na_br` varchar(50) NOT NULL,
-  `su_datum` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `sacuvane_uplatnice`
---
-
-INSERT INTO `sacuvane_uplatnice` (`id_uplate`, `su_id_uplatioca`, `su_uplatilac`, `su_svrha`, `su_primalac`, `su_sifra`, `su_valuta`, `su_iznos`, `su_racun_primaoca`, `su_model`, `su_poz_na_br`, `su_datum`) VALUES
-(2, 1, 'Aleksandar Zivanovic Rudarska 5 12000 Pozarevac', 'Doprinos za zdravstveno osiguranje za 6 meseci', 'ZZZO', 153, 'RSD', '15945,00', '8400000721432843284334', '97', '97-080-1127400676568', '2021-01-17 23:22:08'),
-(3, 1, 'Aleksandar Zivanovic Rudarska 5 12000 Pozarevac', 'id = 3', 'id = 3', 123, 'RSD', '3', '12345', '123', ' ', '2021-01-18 22:43:00'),
-(5, 1, 'Aleksandar Zivanovic Rudarska 5 12000 Pozarevac', 'pokusaj 2. - bez modela i poz na br - posle ispravke imena varijabli', '123', 153, 'RSD', '15945,00', '123', ' ', ' ', '2021-01-19 00:03:50'),
-(6, 1, 'Aca Zivanovic', 'pokusaj 2. - bez modela i poz na br - posle ispravke imena varijabli', '123', 153, 'RSD', '15945,00', '123', ' ', ' ', '2021-01-19 00:04:30'),
-(8, 1, 'Aleksandar Zivanovic Rudarska 5 12000 Pozarevac', 'erwr', 'ewrerw', 153, 'RSD', '234', '8400000721432843284334', '123', ' ', '2021-01-19 00:09:37'),
-(10, 1, 'Aleksandar Zivanovic Rudarska 5 12000 Pozarevac', 'testiranje bez modela i poz na br', 'primalac', 153, 'RSD', '10056', '123', ' ', 'sdasdasda', '2021-01-19 14:54:14'),
-(14, 1, 'Aleksandar Zivanovic Rudarska 5 12000 Pozarevac', 'id = 2', 'id = 2', 123, 'RSD', '13', '12345', '2', '2', '2021-01-19 15:07:55');
-
--- --------------------------------------------------------
+  `su_datum` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id_uplate`),
+  UNIQUE KEY `kod_uplatnice` (`kod_uplatnice`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `uplatilac`
 --
 
+DROP TABLE IF EXISTS `uplatilac`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `uplatilac` (
-  `id_uplatioca` int(11) NOT NULL,
+  `id_uplatioca` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `email_uplatioca` varchar(75) NOT NULL,
   `password_uplatioca` varchar(255) NOT NULL,
@@ -84,84 +58,20 @@ CREATE TABLE `uplatilac` (
   `adresa_uplatioca` varchar(255) NOT NULL,
   `postanski_br_uplatioca` int(5) NOT NULL,
   `mesto_uplatioca` varchar(255) NOT NULL,
-  `reset_token` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `reset_token` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_uplatioca`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Dumping data for table `uplatilac`
---
-
-INSERT INTO `uplatilac` (`id_uplatioca`, `username`, `email_uplatioca`, `password_uplatioca`, `ime_uplatioca`, `prezime_uplatioca`, `adresa_uplatioca`, `postanski_br_uplatioca`, `mesto_uplatioca`, `reset_token`) VALUES
-(1, 'AcaZ', 'acas_mail@yahoo.com', '$2y$10$faurG2NUZMUIAw7mHjTeXeiotdT.v.ovqP08w6Dp2DCe5z6qQGybq', 'Aleksandar', 'Zivanovic', 'Rudarska 5', 12000, 'Pozarevac', ''),
-(2, 'pera', 'pera@ptt.rs', '$2y$10$idNIcNEdgDrX9FHt2VhRIu0a.JncvJxHKeCzTYoiPSqWF/R1TOTGy', 'Petar', 'Petrovic', 'Pere Bandere 127', 11000, 'Beograd', ''),
-(3, 'mika', 'mika@mika.com', '$2y$10$idNIcNEdgDrX9FHt2VhRIu0a.JncvJxHKeCzTYoiPSqWF/R1TOTGy', 'Mika', 'Mikic', 'Mike Alasa 23/4 ulaz B', 11000, 'Beograd', ''),
-(4, 'Maksa', '', '', 'Maksim', 'Gorki', 'Maksima Gorkog 6/A', 26000, 'Smederevo', ''),
-(6, 'zika_zmaj', '', '123', 'Zika', 'Zmaj', 'Jove Jovanovica Zmaja 12', 2344412, '&lt;script&gt;alert(\'pera\')&lt;/script&gt;', ''),
-(7, 'Izmenjeni', 'izmenjeni@izmena.com', '123', 'Ime', 'Prezime', 'adresa', 12345, 'Vukojebina', ''),
-(46, 'osoba1', 'osoba1@gmail.com', 'osoba1', 'osoba1', 'osoba1', 'osoba1ghj', 11111111, 'osoba1', ''),
-(51, 'trta', 'trta@trta.com', '$2y$10$idNIcNEdgDrX9FHt2VhRIu0a.JncvJxHKeCzTYoiPSqWF/R1TOTGy', 'trta', 'trta', 'trta', 123, 'trta', ''),
-(52, 'djoka', 'djoka@djoka.com', '$2y$10$VbIDlVmgnQ3qQ5y.l0.emO.hkR/sQNuX1Nj3.RwoM3w/5kBuoJQeG', 'Djoka', 'Djokic', 'Brace Dobrnjac 132', 12000, 'Pozarevac', '13223bc203712e4f6456dc2e4806573dfefce4c4'),
-(54, 'mirko', 'mirko@mirko.com', '$2y$10$LA5qGaJoIXu5TJlofKLsIeQ1LhUdpY9CGWQcvTqyFLskmZrKTAZUS', 'Mirko', 'Mirkovic', 'Cane Babovica bb', 11000, 'Beograd', '5a4ccd0079d9c333b6c73e0e0923cfe3b23400d3'),
-(55, 'slavko', 'slavko@slavko.com', '$2y$10$TXqtagbyUG36xBeS0UTHoOdiByo47RCZiNkNVAz3dbszFwt57s0r2', 'Slavko', 'Slavkovic', 'Neznanog junaka 12', 12000, 'Pozarevac', ''),
-(56, 'draza', 'resetovanjekorisnickesifre@gmail.com', '$2y$10$9dQF45yMFl5gMV.VuIT1aeTB7FbLL0kgaUMaQZjWNembZiSrfhheW', 'Draza', 'Reglaza', 'Hipodrom 12', 11000, 'Beograd', '9186476a0349a7e725642fc562dc7b939c0b3ef1');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `primalac`
---
-ALTER TABLE `primalac`
-  ADD PRIMARY KEY (`id_primaoca`),
-  ADD KEY `fk_id_uplatioca` (`id_uplatioca`);
-
---
--- Indexes for table `sacuvane_uplatnice`
---
-ALTER TABLE `sacuvane_uplatnice`
-  ADD PRIMARY KEY (`id_uplate`);
-
---
--- Indexes for table `uplatilac`
---
-ALTER TABLE `uplatilac`
-  ADD PRIMARY KEY (`id_uplatioca`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `primalac`
---
-ALTER TABLE `primalac`
-  MODIFY `id_primaoca` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `sacuvane_uplatnice`
---
-ALTER TABLE `sacuvane_uplatnice`
-  MODIFY `id_uplate` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
-
---
--- AUTO_INCREMENT for table `uplatilac`
---
-ALTER TABLE `uplatilac`
-  MODIFY `id_uplatioca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `primalac`
---
-ALTER TABLE `primalac`
-  ADD CONSTRAINT `fk_id_uplatioca` FOREIGN KEY (`id_uplatioca`) REFERENCES `primalac` (`id_primaoca`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-31  3:31:45
